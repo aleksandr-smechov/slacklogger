0e76fea7016f0cd9bc202279eff9935849247bf5
Aleksandr Smechov <aleksvietnam@gmail.com>
2020-10-07 13:36:24 -0400

    finalizing first draft

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="slacklogger",
-    version="1.0.0",
+    version="0.0.1",
     description="Application-level event tracking with Slack",
     long_description=README,
     long_description_content_type="text/markdown",
-    author="Aleks Smechov",
-    author_email="aleks@smechov.com",
+    author="Aleks",
+    author_email="aleks@extractorapi.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
